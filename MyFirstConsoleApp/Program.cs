eadd5a63d08e4e005faf987021c3666d4dedb808
Faruqe <moadmin@Mo-Faruqes-NBoC-Macbook-Pro.local>
2025-08-25 14:47:45 -0400

    first commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first console application in C#.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Dot Net Practice
+Multiple projects
